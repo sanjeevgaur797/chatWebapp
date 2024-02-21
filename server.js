3598b65930cc6b1a77a0f94527f0b70266f888a1
iosoctal <ios@octalsoftware.com>
2024-02-21 15:21:32 +0530

    add using diffrent way

--- server.js
+++ server.js
@@ -4,17 +4,8 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const moment = require("moment");
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 app.use(express.static(path.join(__dirname + "/public")));
